34f1f26298439974caa61fb1bd8a8e3cdbfce0dc
Jelson Santos <jesantos0527@gmail.com>
2017-05-14 11:44:47 -0700

    latest changes for clustering

--- app/models/query.js
+++ app/models/query.js
@@ -3,6 +3,10 @@ var dbconnection      =   require('../dbconnection.js');
 var stateVariables    =   require("../state_variables");
 var ejs				  =	  require('ejs');
 
+
+var kmeans = require('node-kmeans');
+var kmean = require('kmeans');
+
 dbconnection.connect(function(err){
  if(!err) {
      console.log("Database is connected ...");  
@@ -11,7 +15,47 @@ dbconnection.connect(function(err){
  }
  });
 
+function testing(){
+	var data = [
+		  {'company': 'Microsoft' , 'size': 91259, 'revenue': 60420},
+		  {'company': 'IBM' , 'size': 400000, 'revenue': 98787},
+		  {'company': 'Skype' , 'size': 700, 'revenue': 716},
+		  {'company': 'SAP' , 'size': 48000, 'revenue': 11567},
+		  {'company': 'Yahoo!' , 'size': 14000 , 'revenue': 6426 },
+		  {'company': 'eBay' , 'size': 15000, 'revenue': 8700},
+		];
+
+	let vectors = new Array();
+	for (let i = 0 ; i < data.length ; i++) {
+	  vectors[i] = [ data[i]['size'] , data[i]['revenue']];
+	}
+ 	//console.log(vectors)
+	// var kmeans = require('node-kmeans');
+	kmeans.clusterize(vectors, {k: 2}, (err,res) => {
+	  if (err) console.error(err);
+	  else {
+	  	//console.log('%o',res[0]);}
+	  	console.log('clusterize');
+	  }
+	});
+	console.log('HEEERERERERFERERE')
+	//var k = require('kmeans');
+	var km = kmean.create([
+	  [1, 2],
+	  [5, 4],
+	  [2, 5],
+	  [8, 4]
+	], 2);
+
+	var result = km.process();
+	console.log(result);
+
+	//console.log(result.means);
+	//console.log(result.clusters);
+}
+
 exports.dbQuery = function (req, callback){
+	testing()
  	var state = req.params.state
  	var adId = req.params.ad_id
 
@@ -22,10 +66,29 @@ exports.dbQuery = function (req, callback){
  		if(rows.length == 0){
  			console.log("This entry doesn't exist. Processing all queries.")
  			var insertTemp1 = 'create table temp1 as (select display_id from clicked_ads where clicked_ad_id = '+adId+')'
- 			dbconnection.query(insertTemp1, function(err, rows, fields) {
- 				var insertTemp2 = 'create table temp2 as (select display_id from '+ state +'_events)'
- 				dbconnection.query(insertTemp2, function(err, rows, fields) {
- 					processNewQueries(adId, state, callback)
+ 			dbconnection.query(insertTemp1, function(err1, rows1, fields1) {
+ 				var insertTemp2 = 'create table temp2 as (select display_id, hour, minute from '+ state +'_events)'
+ 				console.log("Finished creating temp1.")
+ 				dbconnection.query(insertTemp2, function(err2, rows2, fields2) {
+ 					//'(select * from temp2) as t2 ' +
+ 					var createInnerJoinTemp3 = 'create table temp3 as (select t2.display_id, t2.hour, t2.minute from ' + 
+						'(select * from temp1) as t1 ' +
+						'inner join ' +
+						'(select display_id, hour, minute from '+ state +'_events) as t2 ' +
+						'on t1.display_id = t2.display_id)'
+					console.log("Finished creating temp2.")
+					dbconnection.query(createInnerJoinTemp3, function(err3, rows3, fields3) {
+						var createTemp4 = 'create table temp4 as (select t4.display_id, t4.hour, t4.minute from '+
+							'(select * from temp1) as t1 '+
+							'inner join '+
+							'(select display_id, hour, minute from US_events) as t4 '+
+							'on t1.display_id = t4.display_id)'
+						console.log("Finished creating temp3.")
+ 						dbconnection.query(createTemp4, function(err4, rows4, fields4) {
+ 							console.log("Finished creating temp4.")
+ 							processNewQueries(adId, state, callback)
+ 						});
+ 					});
  				});
  			});
  		}else{
@@ -45,11 +108,11 @@ function performQueryCount(query, name, callback){
 			console.log(err);
 		}else{
 			console.log("QUERY: "+ query)
-			console.log("ROWS: "+ rows)
+			//console.log("ROWS: "+ rows[0])
 			var parser = JSON.parse(JSON.stringify(rows))
 			//var key = "'" + name +"'"
 			//console.log(name)
-			var count = parser[0].count
+			//var count = -1//parser[0].count
 			var data = {}
 			if(name == 'clicks_per_state'){
 				data = {'clicks_per_state': count}
@@ -60,7 +123,24 @@ function performQueryCount(query, name, callback){
 			}else if(name == 'total_clicks'){
 				data = {'total_clicks': count}
 			}else if(name == 'ctr'){
+				var count = parser[0].count
 				data = {'ctr': count}
+			}else if(name == 'state_cluster'){
+				var array_2D = [];
+				for (var i in rows) {
+        			console.log('HOUR: ', rows[i].hour);
+        			console.log('MINUTE: ', rows[i].minute);
+        			array_2D.push([rows[i].hour, rows[i].minute])
+   	 			}
+   	 			data = {'state_cluster': array_2D}
+			}else if(name == 'us_cluster'){
+				var array_2D = [];
+				for (var i in rows) {
+        			console.log('HOUR: ', rows[i].hour);
+        			console.log('MINUTE: ', rows[i].minute);
+        			array_2D.push([rows[i].hour, rows[i].minute])
+   	 			}
+   	 			data = {'us_cluster': array_2D}
 			}
 			console.log("Finished performing query: " + query)
 	        callback(err, data)
@@ -69,7 +149,7 @@ function performQueryCount(query, name, callback){
 
 }
 
-function processNewQueries(adId, state, callback){
+function processNewQueries(adId, state, callback_res){
 	// var queryStateGivenClickedAd = 'select count(*) as count from ' + 
 	// 	'(select t1.display_id from (select display_id from clicked_ads where clicked_ad_id = '+ adId +')t1) as temp1 ' +
 	// 	'inner join ' +
@@ -83,17 +163,26 @@ function processNewQueries(adId, state, callback){
 	// 	'(select * from '+ state +'_events) as temp2 ' +
 	// 	'on temp1.display_id = temp2.display_id)'
 
-	var queryStateGivenClickedAd = 'select count(*) as count from ' + 
-		'(select * from temp1) as t1 ' +
-		'inner join ' +
-		'(select * from temp2) as t2 ' +
-		'on t1.display_id = t2.display_id'
+	// var queryStateGivenClickedAd = 'create table temp3 as (select * from ' + 
+	// 	'(select * from temp1) as t1 ' +
+	// 	'inner join ' +
+	// 	'(select * from temp2) as t2 ' +
+	// 	'on t1.display_id = t2.display_id)'
 
+	// var queryStateGivenClickedAd = 'select count(*) as count from ' + 
+	// 	'(select * from temp1) as t1 ' +
+	// 	'inner join ' +
+	// 	'(select * from temp2) as t2 ' +
+	// 	'on t1.display_id = t2.display_id'
+
+	var queryStateGivenClickedAd = 'select count(*) as count from temp3'
 	//var queryState = 'select count(*) as count from '+ state +'_events'
 	var queryState = 'select count(*) as count from temp2'
 	//var queryClicks = 'select count(*) as count from clicked_ads where clicked_ad_id = '+ adId
 	var queryClicks = 'select count(*) as count from temp1'
 	var queryCtr = 'select ctr as count from click_rate where ad_id = '+ adId
+	var queryStateCluster = 'select hour, minute from temp3'
+	var queryUSCluster = 'select hour, minute from temp4'
 
 	///////////////////////
 	/// Remove query since this number doesn't change and we get better performanace
@@ -119,14 +208,22 @@ function processNewQueries(adId, state, callback){
     	performQueryCount(queryCtr, 'ctr', callback5)
 	}
 
+	var functionStateCluster = function(callback6){
+    	performQueryCount(queryStateCluster, 'state_cluster', callback6)
+	}
+	var functionUSCluster = function(callback7){
+    	performQueryCount(queryUSCluster, 'us_cluster', callback7)
+	}
 	// var query_arr = [
  //    	functionGivenClicked, functionState, functionUS, functionClicks, functionCtr
 	// ]
 
+	// var query_arr = [
+ //    	functionGivenClicked, functionState, functionClicks, functionCtr, functionStateCluster, functionUSCluster
+	// ]
 	var query_arr = [
-    	functionGivenClicked, functionState, functionClicks, functionCtr
+    	functionCtr, functionStateCluster, functionUSCluster
 	]
-
  	async.parallel(query_arr, function (err, result) {
  		var temp = {}
  		for (i in result){
@@ -135,22 +232,116 @@ function processNewQueries(adId, state, callback){
  				temp[keys[key]] = result[i][keys[key]]
  			}
  		}
- 		var data = {'ad_id': adId, 'state': state, 'total_clicks': temp.total_clicks, 'clicks_per_state':temp.clicks_per_state, 
- 				   'total_us':18595452, 'total_state': temp.total_state, 'ctr':temp.ctr}
-		var queryInsert = 'INSERT into counts_per_ad_id_and_state '+
-			'(ad_id,state,total_clicks,clicks_per_state,total_us,total_state,ctr)' +
-			'values '+
-			'(' + adId +',"'+state+'",'+temp.total_clicks+','+temp.clicks_per_state+',18595452,'+temp.total_state+','+temp.ctr+')'
+ 		// var data = {'ad_id': adId, 'state': state, 'total_clicks': temp.total_clicks, 'clicks_per_state':temp.clicks_per_state, 
+ 		// 		   'total_us':18595452, 'total_state': temp.total_state, 'ctr':temp.ctr}
+ 		// var data = {'ad_id': adId, 'state': state, 'total_clicks': "temp.total_clicks", 'clicks_per_state':"temp.clicks_per_state", 
+ 		// 		   'total_us':18595452, 'total_state': "temp.total_state", 'ctr':temp.ctr}
+ 		var clusterObject;
+ 		var testCluster = kmeans.clusterize(temp.state_cluster, {k: 2}, (err,res) => {
+	  		if (err){
+	  			console.error(err);
+	  		}else{
+	  			console.log("----------------------------------------------------------------------------------")
+	  			console.log('%o',res);
+	  			clusterObject = res
+	  			console.log("----------------------------------------------------------------------------------")
+	  		}
+		});
+		console.log(testCluster)
+ 		var data = {'ad_id': adId, 'state': state, 'state_cluster': temp.state_cluster, 'us_cluster':temp.us_cluster, 
+ 				   'ctr':temp.ctr}
+ 		//callback(err,data)
+ 	// 	kmeans.clusterize(temp.state_cluster, {k: 2}, (err,res) => {
+	 //  		if (err){
+	 //  			console.error(err);
+	 //  		}else{
+	 //  			console.log("----------------------------------------------------------------------------------")
+	 //  			console.log('%o',res);
+	 //  			console.log("----------------------------------------------------------------------------------")
+	 //  		}
+		// });
 
- 		dbconnection.query(queryInsert, function(err, rows, fields) {
- 			var deleteTable1 = 'drop table temp1'
- 			dbconnection.query(deleteTable1, function(err1, rows1, fields1) {
- 				var deleteTable2 = 'drop table temp2'
- 				dbconnection.query(deleteTable2, function(err1, rows1, fields1) {
- 					callback(err,data)
- 				});
- 			});
- 		});
+		var queryInsert = 'INSERT into counts_per_ad_id_and_state '+
+			'(ad_id,state,state_cluster,us_cluster,ctr) ' +
+			'values '+
+			'(' + adId +',"'+state+'",'+temp.state_cluster+','+temp.us_cluster+','+temp.ctr+')'
+			
+		//,total_clicks,clicks_per_state,18595452,total_state,'+temp.ctr+')'
+
+		//'(' + adId +',"'+state+'",'+"temp.total_clicks"+','+"temp.clicks_per_state"+',18595452,'+"temp.total_state"+','+temp.ctr+')'
+		// console.log("INSERT: " + queryInsert)
+
+		// var deleteTables = 'drop table temp1, temp2, temp3, temp4'
+ 	// 		console.log("Finished inserting data for future queries.")
+ 	// 		console.log(data)
+ 	// 		dbconnection.query(deleteTables, function(err1, rows1, fields1) {
+ 	// 			console.log("Finished deleting temp tables.")
+ 	// 			callback_res(err,data)
+ 	// 		});
+
+ 		var deleteTables = 'drop table temp1, temp2, temp3, temp4'
+		console.log(data)
+		dbconnection.query(deleteTables, function(err1, rows1, fields1) {
+			console.log("Finished deleting temp tables.")
+			//callback_res(err,data)
+			//var clusterObject;
+
+
+			var km_us = kmean.create(temp.us_cluster, 2);
+
+			var clusters_us = km_us.process();
+			//console.log(result);
+
+			var km_state = kmean.create(temp.state_cluster, 2);
+			var clusters_state = km_state.process();
+
+			var data1 = {'ad_id': adId, 'state': state, 'state_cluster': temp.state_cluster, 'us_cluster':temp.us_cluster, 
+			  			'ctr':temp.ctr, 'cluster': clusterObject, 'stCluster': clusters_state, 'usCluster': clusters_us}
+
+			callback_res(err, data1)
+
+
+	 	// 	kmeans.clusterize(temp.state_cluster, {k: 2}, (errkmeans,reskmeans) => {
+		 //  		if (err){
+		 //  			console.error(errkmeans);
+		 //  		}else{
+		 //  			console.log("----------------------------------------------------------------------------------")
+		 //  			console.log('%o',reskmeans);
+		 //  			clusterObject = reskmeans
+		 //  			console.log("----------------------------------------------------------------------------------")
+		 //  		}
+		 //  		stCluster = reskmeans
+			// 	//callback_res(err,data)
+			// 	kmeans.clusterize(temp.us_cluster, {k: 2}, (errkmeans,reskmean) => {
+			//   		if (err){
+			//   			console.error(errkmean);
+			//   		}else{
+			//   			console.log("----------------------------------------------------------------------------------")
+			//   			console.log('%o',reskmean);
+			//   			clusterObject = reskmean
+			//   			console.log("----------------------------------------------------------------------------------")
+			//   		}
+			//   		usCluster = reskmean
+			// 		//callback_res(err,data)
+			// 		//console.log(stateCluster)
+			//  		var data1 = {'ad_id': adId, 'state': state, 'state_cluster': temp.state_cluster, 'us_cluster':temp.us_cluster, 
+			//  			'ctr':temp.ctr, 'cluster': clusterObject, 'stCluster': stCluster, 'usCluster': usCluster}
+			//  		callback_res(err, data1);
+			// 	});
+			// });
+	 		
+		});
+
+ 		// dbconnection.query(queryInsert, function(err, rows, fields) {
+ 		// 	var deleteTables = 'drop table temp1, temp2, temp3, temp4'
+ 		// 	console.log("Finished inserting data for future queries.")
+ 		// 	console.log(err)
+ 		// 	console.log(data)
+ 		// 	dbconnection.query(deleteTables, function(err1, rows1, fields1) {
+ 		// 		console.log("Finished deleting temp tables.")
+ 		// 		callback_res(err,data)
+ 		// 	});
+ 		// });
  	});
 
 }
--- app/server.js
+++ app/server.js
@@ -26,6 +26,16 @@ router.get("/prediction.html",function(req,res){
     res.sendFile(path.join(__dirname + '/prediction.html'));
 });
 
+router.get("/cluster.html",function(req,res){
+    //res.json({"error" : false,"message" : "Hello World"});
+    res.sendFile(path.join(__dirname + '/cluster.html'));
+});
+
+router.get("/cluster_testing.html",function(req,res){
+    //res.json({"error" : false,"message" : "Hello World"});
+    res.sendFile(path.join(__dirname + '/cluster_testing.html'));
+});
+
 router.get("/file4/state/:state/start_date/:start_date/end_date/:end_date/start_time/:start_time/end_time/:end_time",function(req,res){
     console.log("file4 request")
     queryModel.file_4(req, function(err, data){
@@ -40,11 +50,17 @@ router.get("/file4/state/:state/start_date/:start_date/end_date/:end_date/start_
 });
 
 router.get("/api/state/:state/ad_id/:ad_id",function(req,res){
+    var time_0 = Date.now()/1000
+    console.log(time_0)
     queryModel.dbQuery(req, function(err, data){
         if(err){
             //res.json({"error" : true,"message" : "Error processing the query."});
             res.status(500).send("Error processing the query.");
         }else{
+            //res.status(200).json(data);
+            var time_1 = Date.now()/1000
+            var seconds = time_1 - time_0
+            console.log("Request was processed in " + seconds + " seconds.")
             res.status(200).json(data);
         }
     });
@@ -55,8 +71,10 @@ router.get("/api/state/:state/ad_id/:ad_id",function(req,res){
 app.use('/',router);
 
 var port = process.env.PORT || 5000;
-app.listen(port);
+var server = app.listen(port);
 console.log("Listening to PORT "+ port);
 
 var http = require ('http');
+
+server.timeout = 500000;  
 
